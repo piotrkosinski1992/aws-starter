6562fe68254dfa9e16d049d4e7f9fc26b5da62cc
Piotr Kosinski <piotr.kosinski@allegro.pl>
2022-11-05 17:25:17 +0100

    Initial commit

---
+++ src/main/java/com/aws/awsstarter/AwsStarterApplication.java
@@ -0,0 +1,13 @@
+package com.aws.awsstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aws/awsstarter/AwsStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.awsstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
